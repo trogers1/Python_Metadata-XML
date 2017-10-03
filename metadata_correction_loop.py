41464c3416d32597be8acca268fa469395f26f64
Taylor Rogers <taylor.rogers71@gmail.com>
2017-10-03 14:31:39 -0700

    removed metadata_correction_loop.py

--- metadata_correction_loop.py
+++
@@ -1,6 +0,0 @@
-#maybe use terminal to iterate the .py program for each file.
-#use the etree function from the lxml module to find the right tag in the metadata file
-#then use if, elif, else thing.
-
-if len(filepart) < 1:
-    .write ("Whatever it is that I need to write") 
